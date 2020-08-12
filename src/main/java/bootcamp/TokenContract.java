b70632708f5a9ed5bcb956e344644bacb1ec5070
kj-r3 <kevin.jaquez@r3.com>
2020-08-12 05:15:35 -0400

    Checkpoint - CordApp * Defined TokenContract * Defined TokenState * Added TokenStateUtilities * Enabled StateTests * Enabled ContractTests

--- src/main/java/bootcamp/TokenContract.java
+++ src/main/java/bootcamp/TokenContract.java
@@ -1,24 +1,143 @@
 package bootcamp;
 
 import net.corda.core.contracts.CommandData;
+import net.corda.core.contracts.CommandWithParties;
+import net.corda.core.contracts.Contract;
+import net.corda.core.identity.Party;
 import net.corda.core.transactions.LedgerTransaction;
-import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;    
+import org.jetbrains.annotations.NotNull;
+
+import java.security.PublicKey;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static net.corda.core.contracts.ContractsDSL.requireSingleCommand;
 import static net.corda.core.contracts.ContractsDSL.requireThat;
 
-import java.util.List;
+// ************
+// * Contract *
+// ************
 
-/* Our contract, governing how our state will evolve over time.
- * See src/main/java/examples/ArtContract.java for an example. */
-public class TokenContract {
-    public static String ID = "bootcamp.TokenContract";
+/**
+ * Implementation of a basic token contract in Corda
+ *
+ * This contract enforces rules regarding the creation of tokens [TokenState], which encapsulates tokens
+ *
+ * For a new [Token] to be issued, a transaction is required which takes:
+ *  - Zero input states.
+ *  - One or more output states:  The new [Token]
+ *  - A Issue() command with the public keys of the issuers of the token
+ *
+ *  For a [Token] to be transferred, a transaction is required which takes:
+ *  - One input state (issued tokens) that has not been consumed
+ *  - Two output state: One output for the issuer of the move and another for the holder
+ *  - A Move() command with the public keys of the issuer (initiator) and the holder (receiver) of the token
+ *
+ *  For a [Token] to be redeemed, a transaction is required which takes:
+ *  - One input state (issued tokens) that has not been consumed
+ *  - One output state: One output just for the issuer (initiator) removing the quantity of the tokens
+ *  - A Redeem() command with the public key of the issuer
+ */
+public final class TokenContract implements Contract {
+    // This is used to identify our contract when building a transaction.
+    public static final String TOKEN_CONTRACT_ID = "bootcamp.TokenContract";
 
+    // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
+    // does not throw an exception.
+    @Override
+    public void verify(@NotNull final LedgerTransaction tx) {
+        final CommandWithParties<Commands> command = requireSingleCommand(tx.getCommands(), Commands.class);
 
-    public void verify(LedgerTransaction tx) throws IllegalArgumentException {
-    
+        // This contract does not care about states it has no knowledge about
+        // The following variables are used for various verifications
+        final List<TokenState> inputs = tx.inputsOfType(TokenState.class);
+        final List<TokenState> outputs = tx.outputsOfType(TokenState.class);
+        final boolean hasPositiveQuantities =
+                inputs.stream().allMatch(it -> 0 < it.getQuantity()) &&
+                        outputs.stream().allMatch(it -> 0 < it.getQuantity());
+        final Set<PublicKey> allInputHolderKeys = inputs.stream()
+                .map(it-> it.getHolder().getOwningKey())
+                .collect(Collectors.toSet());
+
+        if(command.getValue() instanceof Commands.Issue){
+            requireThat(require -> {
+                //Generic constraints around the Issue Token command.
+                require.using("No tokens should be consumed, in inputs, when creating a token.",
+                        inputs.isEmpty());
+                require.using("Token output state should be created when issuing new tokens.",
+                        !outputs.isEmpty());
+
+                //Constraints of the tokens themselves.
+                require.using("All quantities must be above 0.",
+                        hasPositiveQuantities);
+
+                //Constraints on the signers of the issuance.
+                require.using("The issuers should sign.",
+                        command.getSigners().containsAll(outputs.stream()
+                                .map(it -> it.getIssuer().getOwningKey())
+                                .collect(Collectors.toSet())
+                        ));
+
+                return null;
+            });
+        } else if(command.getValue() instanceof Commands.Move) {
+            requireThat(require -> {
+                //Generic constraints around the Move Token command.
+                require.using("At least one inputs should be consumed when moving tokens.,",
+                        !inputs.isEmpty());
+                require.using("At least one output should be created when moving tokens.",
+                        !outputs.isEmpty());
+
+                //Constraints of the tokens themselves.
+                require.using("All quantities must be above 0.",
+                        hasPositiveQuantities);
+                final Map<Party, Long> inputSums = TokenStateUtilities.mapSumByIssuer(inputs);
+                final Map<Party, Long> outputSums = TokenStateUtilities.mapSumByIssuer(outputs);
+                require.using("The list of issuers should be conserved.",
+                        inputSums.keySet().equals(outputSums.keySet()));
+                require.using("The sum of quantities for each issuer should be conserved.",
+                        inputSums.entrySet().stream()
+                                .allMatch(entry -> outputSums.get(entry.getKey()).equals(entry.getValue())));
+
+                //Constraints on the signers of the move.
+                require.using("The current holders should sign.",
+                        command.getSigners().containsAll(allInputHolderKeys));
+
+                return null;
+            });
+        } else if (command.getValue() instanceof Commands.Redeem){
+            requireThat(require -> {
+                //Generic constraints around the Redeem Token command.
+                require.using("Inputs should be consumed when redeeming Tokens,",
+                        !inputs.isEmpty());
+                require.using("No output states should be created when redeeming Tokens.",
+                        outputs.isEmpty());
+
+                //Constraints around the tokens themselves
+                require.using("All quantities must be above 0.", hasPositiveQuantities);
+
+                //Constraints on the signers.
+                require.using("The issuers should sign.",
+                        command.getSigners().containsAll(inputs.stream()
+                                .map(it -> it.getIssuer().getOwningKey())
+                                .collect(Collectors.toSet())
+                        ));
+                require.using("The current holders should sign.",
+                        command.getSigners().containsAll(allInputHolderKeys));
+
+                return null;
+            });
+        } else {
+            throw new IllegalArgumentException("Unknown command " + command.getValue());
+        }
     }
 
-
+    // Used to indicate the transaction's intent.
     public interface Commands extends CommandData {
-        class Issue implements Commands { }
+        class Issue implements Commands {}
+        class Move implements Commands {}
+        class Redeem implements Commands {}
     }
 }
--- src/main/java/bootcamp/TokenState.java
+++ src/main/java/bootcamp/TokenState.java
@@ -4,14 +4,78 @@ import net.corda.core.contracts.BelongsToContract;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.Party;
+import net.corda.core.serialization.ConstructorForDeserialization;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
-/* Our state, defining a shared fact on the ledger.
- * See src/main/java/examples/ArtState.java for an example. */
-//@BelongsToContract(TokenContract.class)
-public class TokenState {
+@BelongsToContract(TokenContract.class)
+public final class TokenState implements ContractState {
 
+    @NotNull
+    private final Party issuer;
+    @NotNull
+    private final Party holder;
+    private final long quantity;
+
+    @ConstructorForDeserialization
+    public TokenState(@NotNull final Party issuer, @NotNull final Party holder, final long quantity) {
+        //noinspection ConstantConditions
+        if (issuer == null) throw new NullPointerException("issuer cannot be null");
+        //noinspection ConstantConditions
+        if (holder == null) throw new NullPointerException("holder cannot be null");
+        this.issuer = issuer;
+        this.holder = holder;
+        this.quantity = quantity;
+    }
+
+    @NotNull
+    @Override
+    public List<AbstractParty> getParticipants() {
+        return Arrays.asList(issuer, holder);
+    }
+
+    @NotNull
+    public Party getIssuer() {
+        return issuer;
+    }
+
+    @NotNull
+    public Party getHolder() {
+        return holder;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    // Forgetting equals and hashcode will cause all sorts of nasty side effects, as we are likely to put instances
+    // in Sets or HashMaps. You always want to be able to know whether 2 instances are the same anyway.
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final TokenState that = (TokenState) o;
+        return quantity == that.quantity &&
+                issuer.equals(that.issuer) &&
+                holder.equals(that.holder);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(issuer, holder, quantity);
+    }
+
+    @Override
+    public String toString() {
+        return "TokenState{" +
+                "issuer=" + issuer +
+                ", holder=" + holder +
+                ", quantity=" + quantity +
+                '}';
+    }
 }
